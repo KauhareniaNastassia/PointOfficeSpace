146d25ff233e672ca4dcef8e554a6466fb925a79
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-03-28 22:53:30 +0300

    add animation to 1-4 blocks

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     fix100vh();
     findHeight();
+    animation();
     window.addEventListener('resize', () => {
         fix100vh();
         findHeight();
@@ -47,3 +48,70 @@ function findHeight(){
     } 
 }
 
+
+function animation() {
+    gsap.registerPlugin(ScrollTrigger)
+
+    const tlTitle = gsap.timeline({})
+   
+    tlTitle.from('.promo__title span:first-child', {
+        xPercent: 200,
+        ease: 'back.out(1.1)',
+        duration: 1.5,
+    })
+    tlTitle.from('.promo__title span:last-child', {
+        xPercent: -200,
+        ease: 'back.out(1.1)',
+        duration: 1.5,
+    }, '<')
+
+
+    const tlPromo = gsap.timeline({
+        ScrollTrigger: {
+            trigger: '.promo',
+            start: 'top top',
+            end: '+=50%',
+            scrub: 1,
+            pin: true
+        }
+    })
+    tlPromo.from('promo__bottom', {
+        opacity: 0,
+        yPercent: 100
+    })
+    .fromTo('.promo__bottom img', {y: -155}, {
+        y: 30
+    }, '<')
+
+
+    const tlLines = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.choose__wrap',
+            start: 'top bottom',
+            end: 'bottom top',
+            scrub: true,
+        }
+    })
+    tlLines.to('.choose__wrap .top', {
+        xPercent: -70,
+    })
+    tlLines.to('.choose__wrap .bottom', {
+        xPercent: 30,
+    }, '<')
+
+
+    gsap.from('.rates__card', {
+        duration: 1,
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.3,
+        scrollTrigger: {
+            trigger: '.rates__wrap',
+            start: 'top bottom',
+            toggleActions: 'play none none reverse'
+        }
+    })
+
+
+
+}
